b536668e5967e3ed57c05c7f7c592ff4ee6b20e0
FakhrEldeen <fakhreldeengamal@gmail.com>
2018-01-12 01:05:31 +0200

    Update Key Generation.py

--- Key Generation.py
+++ Key Generation.py
@@ -1,11 +1,19 @@
+#This Code written by Fakhr Eldeen Gamal 
+#This is Key Generation Functions for DES Encryption Algorithm 
+
+
+
+#Values of First Permutation Box 
 PC1 = [57, 49, 41, 33, 25, 17, 9, 1, 58, 50, 42, 34, 26, 18, 10, 2, 59, 51, 43,
        35, 27, 19, 11, 3, 60, 52, 44, 36, 63, 55, 47, 39, 31, 23, 15, 7, 62, 54,
        46, 38, 30, 22, 14, 6, 61, 53, 45, 37, 29, 21, 13, 5, 28, 20, 12, 4]
 
+#Values of Second Permutation Box
 PC2 = [14, 17, 11, 24, 1, 5, 3, 28, 15, 6, 21, 10, 23, 19, 12, 4, 26, 8, 16, 7,
        27, 20, 13, 2, 41, 52, 31, 37, 47, 55, 30, 40, 51, 45, 33, 48, 44, 49, 39,
        56, 34, 53, 46, 42, 50, 36, 29, 32]
 
+#Rotation Values for each Iteration
 Rotations = [1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1]
 
 # 3 strings to hold the final C's and D's and String to hold the 16-Keys of DES Rounds 
